dab099e0000c8103559988743d83e62068f8f600
aniltks <anil.tako@fusemachines.com>
2019-09-02 10:33:00 +0545

    refactor(index.js): add development enviroment to test

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import './styles/css/custom/style.min.css'
-import Greeting from './lib/'
+import {getRequestParams} from './lib/'
 
 class App extends React.Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            requestParams: {
+                searchText: 'john',
+                page: 1,
+                size: 10,
+            }
+        }
     }
-
 
     render(){
         return(
             <main role="main-content">
                 <section role="content-section">
                     <div className="title">
-                        {Greeting("Create-npm-package-js")}
+                        {getRequestParams(this.state.requestParams)}
                     </div>
                 </section>
             </main>
